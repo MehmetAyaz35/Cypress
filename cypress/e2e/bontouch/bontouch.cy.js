7e46598fa2c1a9cc5807a7129802f4c02a29c896
Mehmet Ayaz <mehmet.90.ayaz@gmail.com>
2024-05-24 19:00:03 +0200

    web elements were located using cypress

--- cypress/e2e/bontouch/bontouch.cy.js
+++ cypress/e2e/bontouch/bontouch.cy.js
@@ -1,33 +1,34 @@
 describe('Bontouch Website Test', () => {
     it('Should apply for a career opportunity', () => {
-        // Ziyaret edilecek URL
+        //  URL
         cy.visit('https://www.bontouch.com/')
         
-        // Çerez bildirimini kapat
-        cy.get('#cookie_action_close_header_reject').click()
+        // close cookies
+        // cy.get('#cookie_action_close_header_reject').click()
         
-        // Kariyer fırsatlarına yönlendir
-        cy.get('body > div.bontouch-site > div.bontouch-site__content > section.bontouch-o-section.bontouch-o-section--no-bottom-padding > div > p > strong > a').click()
+        cy.get('.cm-header-wrapper > .menu-toggle-btn').click()
+
+        cy.get(':nth-child(9) > a > span').click()
         
+        cy.get('[data-action="click->common--cookies--alert#disableAll"]').click()
+
+        cy.get('[href="https://careers.bontouch.com/connect"]').click()
+
+        cy.get('[href="https://careers.bontouch.com/connect"] > .flex').click()
+
+        cy.get('[data-connect--signup-form-target="departments"] > .button').click()
+
+        cy.get('[data-connect--signup-form-target="departments"] > .button').click()
+
+        cy.get('#candidate_consent_given').check()
+
+        cy.get('#candidate_consent_given_future_jobs').check()
+
+        cy.get('#candidate_email').type('mehmet.90.ayaz@gmail.com')
+        cy.wait(1000)
+
+        cy.get('span[class="absolute inset-0 flex items-center justify-center opacity-0 transition transition-opacity').click()
         
-        // İletişim kur
-        cy.get('#section-28338169 > section > div.relative.flex.flex-col.items-center.min-h-\[inherit\].justify-center.w-full.h-full.px-4.text-lg.text-center.z-career-block-content > div > div > a:nth-child(1) > span > span').click()
-        
-        cy.get('body > dialog.fixed.shadow-form.w-auto.items-center.rounded.p-4.bg-company-primary.text-company-primary.overflow-y-auto.border-none.max-h-\[80vh\].z-cookie-alert.lg\:gap-x-6.lg\:max-h-\[calc\(100vh-40px\)\].flex-col.inset-x-4.bottom-4.mb-0.mt-auto.justify-between.\!translate-y-0.\!top-0.lg\:p-10.lg\:flex-row.lg\:inset-x-\[40px\].lg\:mb-6.flex > div.flex.flex-col.items-center.gap-\[10px\].mt-6.flex-shrink-0.mb-2.w-full.md\:flex-row.md\:w-auto.lg\:flex-col > button:nth-child(2)').click()
-        // QA bölümüne tıkla
-        cy.get('#connect > div.flex.min-h-screen.company-links > div.flex.flex-col.items-center.justify-center.w-full.bg-gray-200 > div > div.w-full.max-w-500 > form > div:nth-child(3) > div > div > div > div > div:nth-child(12) > label > span').click()
-        
-        // // Başvuruya devam et
-        // cy.get('#connect form button').click()
-        
-        // // Onay kutularını işaretle
-        // cy.get('#candidate_consent_given').check()
-        // cy.get('#candidate_consent_given_future_jobs').check()
-        
-        // // E-posta adresini gir
-        // cy.get('#candidate_email').type('mehmet.90.ayaz@gmail.com{enter}')
-        
-        // Bekle
-        cy.wait(5000)
+""
     })
 })
